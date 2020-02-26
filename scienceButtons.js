d384dd8573eac33a595f4537df08c46cafad9b3c
Elijah Collins <ecollins152@west-mec.org>
2020-02-26 14:26:41 -0700

    i tried on science

---
+++ scienceButtons.js
@@ -0,0 +1,102 @@
+"use strict";
+
+    var x = document.querySelectorAll("button");
+
+function changeStyle(theme){
+
+    if(theme == 'Original'){
+        document.getElementsByTagName("html")[0].style.background = "url(iceWall.png)no-repeat skyblue";
+        document.getElementsByTagName("html")[0].style.backgroundSize = "120%";
+        document.body.style.background = "#FCFCFF";
+        document.body.style.color = "black";
+        document.getElementsByTagName("p").style.color = "black";
+        document.getElementById("banner").style.background = "";
+        document.getElementById("banner").style.color = "black";
+        document.getElementById("nav").style.background = "#ABD1F3";
+        document.getElementById("sB").style.background = "#ABD1F3";
+        document.getElementById("tea").style.background = "#ABD1F3";
+        document.getElementById("pW").style.background = "#ABD1F3";
+        document.getElementById("bR").style.background = "#ABD1F3";
+        document.getElementById("lemon").style.background = "#ABD1F3";
+        document.getElementById("button1").style.background = "#FCFCFF";
+        document.getElementById("button2").style.background = "#FCFCFF";
+        document.getElementById("button3").style.background = "#FCFCFF";
+        document.getElementById("button4").style.background = "#FCFCFF";
+        document.getElementById("button1").style.color = "black";
+        document.getElementById("button2").style.color = "black";
+        document.getElementById("button3").style.color = "black";
+        document.getElementById("button4").style.color = "black";
+    }
+
+    else if(theme == 'Dawn'){
+        document.getElementsByTagName("html")[0].style.background = "url(iceWall.png)no-repeat skyblue";
+        document.getElementsByTagName("html")[0].style.backgroundSize = "120%";
+        document.body.style.background = "#EA728C";
+        document.body.style.color = "black";
+        document.getElementsByTagName("p").style.color = "black";
+        document.getElementById("banner").style.background = "";
+        document.getElementById("banner").style.color = "black";
+        document.getElementById("nav").style.background = "#4F3961";
+        document.getElementById("sB").style.background = "#4F3961";
+        document.getElementById("tea").style.background = "#4F3961";
+        document.getElementById("pW").style.background = "#4F3961";
+        document.getElementById("bR").style.background = "#4F3961";
+        document.getElementById("lemon").style.background = "#4F3961";
+        document.getElementById("button1").style.background = "#EA728C";
+        document.getElementById("button2").style.background = "#EA728C";
+        document.getElementById("button3").style.background = "#EA728C";
+        document.getElementById("button4").style.background = "#EA728C";
+        document.getElementById("button1").style.color = "black";
+        document.getElementById("button2").style.color = "black";
+        document.getElementById("button3").style.color = "black";
+        document.getElementById("button4").style.color = "black";
+    }
+
+    else if(theme == 'Sunny'){
+        document.getElementsByTagName("html")[0].style.background = "url(iceWall.png)no-repeat skyblue";
+        document.getElementsByTagName("html")[0].style.backgroundSize = "120%";
+        document.body.style.background = "#FFE75E";
+        document.body.style.color = "black";
+        document.getElementsByTagName("p").style.color = "black";
+        document.getElementById("banner").style.background = "";
+        document.getElementById("banner").style.color = "black";
+        document.getElementById("nav").style.background = "#FEB72B";
+        document.getElementById("sB").style.background = "#FEB72B";
+        document.getElementById("tea").style.background = "#FEB72B";
+        document.getElementById("pW").style.background = "#FEB72B";
+        document.getElementById("bR").style.background = "#FEB72B";
+        document.getElementById("lemon").style.background = "#FEB72B";
+        document.getElementById("button1").style.background = "#FFE75E";
+        document.getElementById("button2").style.background = "#FFE75E";
+        document.getElementById("button3").style.background = "#FFE75E";
+        document.getElementById("button4").style.background = "#FFE75E";
+        document.getElementById("button1").style.color = "black";
+        document.getElementById("button2").style.color = "black";
+        document.getElementById("button3").style.color = "black";
+        document.getElementById("button4").style.color = "black";
+    }
+    
+    else if(theme == 'Plum'){
+        document.getElementsByTagName("html")[0].style.background = "url(iceWall.png)no-repeat skyblue";
+        document.getElementsByTagName("html")[0].style.backgroundSize = "120%";
+        document.body.style.background = "#C295D8";
+        document.body.style.color = "black";
+        document.getElementsByTagName("p").style.color = "black";
+        document.getElementById("banner").style.background = "";
+        document.getElementById("banner").style.color = "black";
+        document.getElementById("nav").style.background = "#BE9fE1";
+        document.getElementById("sB").style.background = "#BE9fE1";
+        document.getElementById("tea").style.background = "#BE9fE1";
+        document.getElementById("pW").style.background = "#BE9fE1";
+        document.getElementById("bR").style.background = "#BE9fE1";
+        document.getElementById("lemon").style.background = "#BE9fE1";
+        document.getElementById("button1").style.background = "#C295D8";
+        document.getElementById("button2").style.background = "#FFE75E";
+        document.getElementById("button3").style.background = "#C295D8";
+        document.getElementById("button4").style.background = "#C295D8";
+        document.getElementById("button1").style.color = "black";
+        document.getElementById("button2").style.color = "black";
+        document.getElementById("button3").style.color = "black";
+        document.getElementById("button4").style.color = "black";
+    }
+}
